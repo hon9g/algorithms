3ea4b82d8855e2e8089414f20e0972a056f56ef6
hon9g <mimmh93@gmail.com>
2019-12-07 23:52:12 +0900

    Update quickSort.py

--- Sorting/quickSort.py
+++ Sorting/quickSort.py
@@ -1,27 +1,16 @@
 '''
-1. Arbitrarily, pick the pivot.
-2. partitioning the array to sub-arrays (smaller and bigger part).
+1. Arbitrarily, pick the pivot. (right most one in my code)
+2. partitioning the array to sub-arrays.
 3. recursionally, call quickSort function with sub-arrays.
 '''
 
-def quickSort(A):
-    # Base step
-    if len(A) < 2:
-        return A
+def quickSort1(nums: List[int]) -> List[int]:
+    if len(nums) < 2: return nums
+    pivot = nums[-1]
+    lower = [ x for x in nums if x < pivot ]
+    same = [ x for x in nums if x == pivot ]
+    higher = [ x for x in nums if x > pivot ]
+    return quickSort(lower)+ same + quickSort(higher)
 
-    # Recursion step
-    pivot = A[0]
-    ## partitioning to sub-arrays
-    smaller = []
-    bigger = []
-    for i in range(1, len(A)):
-        if A[i] < pivot:
-            smaller += [A[i]]
-        else:
-            bigger += [A[i]]
-    return quickSort(smaller) + [pivot] + quickSort(bigger)
-
-
-        
 A = [4,1,2]
 print(quickSort(A))
