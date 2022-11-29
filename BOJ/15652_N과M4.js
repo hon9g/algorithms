53426993d38a4dce0202d78e1e4c003d64761ad5
hon9n <mimmh93@gmail.com>
2022-11-29 20:24:19 +0900

    [BOJ 15652] 백트레킹 연습
    O(N ** N * M)

---
+++ BOJ/15652_N과M4.js
@@ -0,0 +1,22 @@
+const BOJ_PATH = 'dev/stdin'
+const LOCAL_PATH = `${__dirname}/stdin/test1.txt`
+
+const input = require('fs').readFileSync(LOCAL_PATH).toString().trim()
+const [N, M] = input.split(' ').map(x => parseInt(x))
+
+const sequence = []
+
+const backtrack = (N, M, result) => {
+  if (result.length === M) {
+    sequence.push(result.join(' '))
+    return
+  }
+  for (let n = result[result.length - 1] ?? 1; n <= N; n++) {
+    backtrack(N, M, [...result, n])
+  }
+}
+
+backtrack(N, M, [])
+
+const sequenceOutfit = sequence.join('\n')
+console.log(sequenceOutfit);
